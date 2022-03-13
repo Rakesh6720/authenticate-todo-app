93811bf2673bab5ed28cd443faf8d6bd57baf6d9
Rakesh Baruah <rbaruah@gmail.com>
2022-03-13 06:15:20 -0500

    completed minified records api call in get todos

--- pages/api/getTodos.js
+++ pages/api/getTodos.js
@@ -5,8 +5,23 @@ var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 
 const table = base(process.env.AIRTABLE_TABLE_NAME);
 
-export default async (req, res) => {
-  const records = await table.select({}).firstPage();
-  res.statusCode = 200;
-  res.json(records);
+const minifyRecords = (records) => {
+  return records.map((record) => getMinifiedRecord(record));
 };
+
+const getMinifiedRecord = (record) => {
+  if (!record.fields.completed) {
+    record.fields.completed = false;
+  }
+  return {
+    id: record.id,
+    fields: record.fields,
+  };
+};
+
+export default async function handler(req, res) {
+  const records = await table.select({}).firstPage();
+  const minifiedRecords = minifyRecords(records);
+  res.statusCode = 200;
+  res.json(minifiedRecords);
+}

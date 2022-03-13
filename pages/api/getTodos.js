204223f992a0e11143aa57338a389a78a7345739
Rakesh Baruah <rbaruah@gmail.com>
2022-03-13 06:01:20 -0500

    added basic get request for todos

---
+++ pages/api/getTodos.js
@@ -0,0 +1,12 @@
+var Airtable = require("airtable");
+var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  process.env.AIRTABLE_BASE_ID
+);
+
+const table = base(process.env.AIRTABLE_TABLE_NAME);
+
+export default async (req, res) => {
+  const records = await table.select({}).firstPage();
+  res.statusCode = 200;
+  res.json(records);
+};

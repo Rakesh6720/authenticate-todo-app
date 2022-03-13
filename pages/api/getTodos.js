4534fa4b04a35ec91ff52f4aae694be4fab9c62f
Rakesh Baruah <rbaruah@gmail.com>
2022-03-13 06:19:00 -0500

    created utils api airtable functions

--- pages/api/getTodos.js
+++ pages/api/getTodos.js
@@ -1,23 +1,4 @@
-var Airtable = require("airtable");
-var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-  process.env.AIRTABLE_BASE_ID
-);
-
-const table = base(process.env.AIRTABLE_TABLE_NAME);
-
-const minifyRecords = (records) => {
-  return records.map((record) => getMinifiedRecord(record));
-};
-
-const getMinifiedRecord = (record) => {
-  if (!record.fields.completed) {
-    record.fields.completed = false;
-  }
-  return {
-    id: record.id,
-    fields: record.fields,
-  };
-};
+import { table, minifyRecords } from "./utils/airtable";
 
 export default async function handler(req, res) {
   const records = await table.select({}).firstPage();
